590dca92653fbe53237fa7329243c8e70d457b6e
2desoo <marcus60.2001@gmail.com>
2024-06-19 02:41:12 +0300

    Changed access modifier

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,8 @@
 import java.util.Objects;
 
 public class Student {
-    String name;
-    int age;
+    private String name;
+    private int age;
 
     public Student(String name, int age) {
         this.name = name;
